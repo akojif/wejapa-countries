c092d9eec9678c9820d300cc2d82ed4d81bde7a8
Akoji Francis <jidwithfa007@crmintcol.org>
2020-08-13 08:22:42 +0100

    Update Countries.vue

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container row">
 
-        <!-- Navbar -->
+    
     <div class="nav">
     <a href="index.html">
       <img src="../assets/logo.png" class="logo"/>
@@ -14,7 +14,7 @@
 
         <div class="column country-info">
             <form>
-                   <!-- Countries Names-->
+            
                     <div class="form-controls">
                         <label for="countries" class="select-label">Select Country</label>
                         <select name="countries" id="countries" v-model="selectedCountry" @change="onCountrySelect($event)" class="count-select">
@@ -23,11 +23,11 @@
                     </div>
                     <br><br>
 
-                    <!-- States Names-->
+            
                     <div class="form-controls">
                         <label for="state" class="select-label">Select State</label>
                         <select name="states" id="states" v-model="selectedState" @change="onStateSelect($event)" class="count-select">
-                            <option :value="state.name" v-for="state in states" :key="state.id" class="count-option">{{ state.name}}</option>
+                            <option :value="state.name" v-for="state in states" :key="state.id" class="count-option">{{ state.name }}</option>
                         </select>
                         <div v-if="stateError" :class="{empty: stateError}">
                             <p>This Country Has No States!</p>
@@ -35,11 +35,10 @@
                     </div>
                     <br><br>
 
-                    <!-- Cities Names -->
                     <div class="form-controls">
                         <label for="cities" class="select-label">Select City</label>
                         <select name="cities" id="cities" v-model="selectedCity" class="count-select">
-                            <option :value="city.name" v-for="city in cities" :key="city.id" class="count-option">{{ city.name}}</option>
+                            <option :value="city.name" v-for="city in cities" :key="city.id" class="count-option">{{ city.name }}</option>
                         </select>
                         <div v-if="cityError" :class="{empty: cityError}">
                             <p>This State Has No Cities!</p>
@@ -48,19 +47,27 @@
             </form>
         </div>
 
-        <!-- Highlighted -->
-        <div class="column-2">
-            <div class="selected-options">
-            <p v-if="selectedCountry"><span class="s-o">Country:</span> {{ selectedCountry }}</p>
-            <br>
-
-            <p v-if="selectedState"><span class="s-o">State:</span> {{ selectedState }}</p>
-            <br>
-
-            <p v-if="selectedCity"><span class="s-o">City:</span> {{ selectedCity }}</p>
-            </div>
-
+    
+       <div class="col-md-6">
+          <div class="card mx-auto" style="width: 22rem;">
+            <div class="card-header text-white" style=" background: #42b983">Details</div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item" v-if="country">
+                <span class="float-left">Country:</span>
+                {{country}}
+              </li>
+              <li class="list-group-item" v-if="state">
+                <span class="float-left">State:</span>
+                {{state}}
+              </li>
+              <li class="list-group-item" v-if="city">
+                <span class="float-left">City :</span>
+                {{city}}
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
 </template>
 
@@ -113,7 +120,6 @@ export default {
                 this.cities = city
                 this.cityError = false
             }
-
         }
     }
 }
@@ -122,7 +128,6 @@ export default {
 <!-- CSS -->
 <style scoped>
 @import url('https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css');
-
 .nav {
   position: relative;
   width: 100%;
@@ -131,12 +136,10 @@ export default {
   margin: 0 !important;
   padding: 0 !important;
 }
-
 .nav a {
     text-decoration: none !important;
     -webkit-tap-highlight-color: transparent !important;
 }
-
 .logo {
   position: relative;
   width: 42px !important;
@@ -145,16 +148,13 @@ export default {
   left: 5%;
   transform: translate(-5%, -50%);
 }
-
 .title {
   text-align: center;
 }
-
 .country-info {
     padding: 20px;
     font-family: 'Gilroy', sans-serif !important;
 }
-
 .count-select  {
   width: 20em;
   height: 2.4em;
@@ -167,41 +167,36 @@ export default {
   border-radius: 6px;
   font-family: 'Gilroy', sans-serif !important;
   font-size: 16px !important;
-  background: url('../assets/dropdown.svg') no-repeat right;
+  background: url('../assets/down.svg') no-repeat right;
   background-size: 22px 22px;
   cursor: pointer;
   -webkit-appearance: none;
   background-position-x: 17.8em;
 }
-
 .count-option {
     display: block;
     margin-left: 20px;
     font-family: 'Gilroy', sans-serif !important;
 }
-
 .column {
     float: left;
     width: 50%;
     padding-left: 20% !important;
-    margin-bottom: 2em !important;
+    margin-bottom: 3em !important;
     height: auto;
 }
-
 .column-2 {
     float: left;
     width: 50%;
     padding-left: 5% !important;
-    margin-bottom: 2em !important;
+    margin-bottom: 3em !important;
     height: auto;
 }
-
 .row:after {
   content: "";
   display: table;
   clear: both;
 }
-
 .empty {
     border: 1px solid;
     background: rgb(226, 64, 64);
@@ -212,22 +207,18 @@ export default {
     border-radius: 6px;
     margin-top: 20px;
 }
-
 .empty p {
     position: relative;
     top: 50% !important;
     transform: translateY(-50%) !important;
 }
-
 .selected-options {
     margin-top: 10px;
 }
-
 .select-label, .s-o {
   font-size: 18px;
   font-weight: bold;  
 }
-
 /* Media Queries */
 /* 1024px */
 @media screen and (max-width: 1024px) {
@@ -236,14 +227,12 @@ export default {
     width: 50%;
     padding-left: 12% !important;
 }
-
 .column-2 {
     float: left;
     width: 50%;
     padding-left: 10% !important;
 }
 }
-
 /* 768px */
 @media screen and (max-width: 768px) {
   .column {
@@ -251,27 +240,24 @@ export default {
     width: 50%;
     padding-left: 8% !important;
 }
-
 .column-2 {
     float: left;
     width: 50%;
     padding-left: 10% !important;
 }
 }
-
 /* 575px */
 @media screen and (max-width: 575px) {
   .column {
     float: left;
     width: 50%;
     padding-left: 8% !important;
-    margin-bottom: 2em !important;
+    margin-bottom: 3em !important;
     height: auto;
 }
-
 .column-2 {
     position: relative;
-    margin-bottom: 2em;
+    margin-bottom: 3em;
     padding-left: 8% !important;
     left: 50%;
     transform: translateX(-50%) !important;
@@ -279,46 +265,38 @@ export default {
     height: auto;
 }
 }
-
 /* 414px */
 @media screen and (max-width: 414px) {
   .column, .column-2 {
     padding-left: 12% !important;
 }
 }
-
 /* 375px */
 @media screen and (max-width: 375px) {
   .column, .column-2 {
     padding-left: 8% !important;
 }
 }
-
 /* 360px */
 @media screen and (max-width: 360px) {
   .column, .column-2 {
     padding-left: 6% !important;
 }
 }
-
 /* 320px */
 @media screen and (max-width: 320px) {
   .title {
     font-size: 28px !important;
   }
-
   .column, .column-2 {
     padding-left: 8% !important;
 }
-
 .count-select {
   width: 17em !important;
   background-position-x: 14.8em;
 }
-
 .empty {
   width: 17em !important;
 }
 }
 </style>
-
